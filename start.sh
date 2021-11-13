64edf7e899ec820169aeb4fa8d534c47269fa74c
Mikhail Lepekhin <supersky2325@gmail.com>
2021-11-13 17:47:52 +0300

    cache server init

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ sudo mysql -e '
     create user if not exists "mike_admin" identified by "mike_admin";
 '
 
-sudo ./build/hl_mai_lab_01 --host=localhost --port=3306 --login=mike_admin --password=1234 --database=person_db
+sudo ./build/hl_mai_lab_01 --host=localhost --port=3306 --login=mike_admin --password=1234 --database=person_db --cache_servers='127.0.0.1:10800,127.0.0.1:10900'
+
 
